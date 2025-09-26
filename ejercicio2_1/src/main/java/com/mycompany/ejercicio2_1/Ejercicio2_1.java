89fb50bfedb34961b6d09030e02811d85860587d
dpelaezv06 <dpelaezv@unal.edu.co>
2025-09-26 09:28:07 -0500

    main con 2 personas
    El main crea 2 personas e imprime sus atributos por consola

---
+++ ejercicio2_1/src/main/java/com/mycompany/ejercicio2_1/Ejercicio2_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio2_1;
+
+/**
+ *
+ * @author daniel
+ */
+public class Ejercicio2_1 {
+
+    
+    public static void main(String[] args) {
+        persona persona_1 = new persona("Pedro", "Perez", "1053121010", 1998 ); // creacion del objeto persona_1
+        persona persona_2 = new persona("Luis", "Leon", "1053223344", 2001 ); // creacion del objeto persona_2
+        persona_1.imprimir(); // mostrar en consola los atributos de la persona 1
+        persona_2.imprimir(); // mostrar en consola los atributos de la persona 2
+    }
+}
