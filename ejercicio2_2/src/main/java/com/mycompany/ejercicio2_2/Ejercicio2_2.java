6c0e63e4bf61453c9795b7a35cde69f8907f714a
dpelaezv06 <dpelaezv@unal.edu.co>
2025-09-26 09:56:01 -0500

    Creacion del proyecto para el ejercicio 2.2

---
+++ ejercicio2_2/src/main/java/com/mycompany/ejercicio2_2/Ejercicio2_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio2_2;
+
+/**
+ *
+ * @author daniel
+ */
+public class Ejercicio2_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
