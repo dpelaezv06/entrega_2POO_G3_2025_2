cebbb6cf69bf73be1da60533ab434f3f01013d38
dpelaezv06 <dpelaezv@unal.edu.co>
2025-09-26 10:07:06 -0500

    definicion del objeto planeta

--- ejercicio2_2/src/main/java/com/mycompany/ejercicio2_2/planeta.java
+++ ejercicio2_2/src/main/java/com/mycompany/ejercicio2_2/planeta.java
@@ -8,6 +8,22 @@ package com.mycompany.ejercicio2_2;
  *
  * @author daniel
  */
+
+ /* esta clase contiene la definicion, el constructor y los atributos de los objetos de tipo planeta */
 public class planeta {
+
+    enum tipos_planetas {GASEOSO, TERRESTRE, ENANO}; // enumeracion para determinar el tipo de planeta
+
+    /* definicion de los atributos de planeta */
+    String nombre = null; // el nombre del planeta
+    int satelites = 0; // cantidad de satelites del planeta
+    double masa = 0; // masa en kilogramos del planeta
+    double volumen = 0; // volumen del planeta en kilometros cubicos
+    int diametro = 0; // diametro del planeta en kilometros
+    int distancia_sol = 0; // distancia entre el planeta y el sol en millones de kilometros
+    tipos_planetas tipo; // tipo de planeta entre gaseoso, terreste o enano
+    boolean observable = false; // el planeta es o no observable a simple vista
     
+       
+
 }
