787a47bd1f23ecb52af6553814d016991afcdfe7
Julián <julperezca@unal.edu.co>
2025-09-28 19:06:38 -0500

    ejercicio2.5 realizado

---
+++ ejercicio2_5/src/main/java/com/company/main_.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import com.company.CuentaBancaria.tipo;
+
+public class main_ {
+    public static void main(String[] args) {
+        CuentaBancaria cuenta = 
+        new CuentaBancaria("Pedro","Pérez", 123456789,tipo.AHORROS);
+        cuenta.imprimir();
+        cuenta.consignar(200000);
+        cuenta.consignar(300000);
+        cuenta.retirar(400000);
+    }
+
+}
