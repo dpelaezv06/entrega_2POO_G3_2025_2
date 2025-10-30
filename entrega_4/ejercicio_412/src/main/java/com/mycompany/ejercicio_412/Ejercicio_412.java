5af21e3f4717e1b80dd61ab19230bbdaa3f272f2
dpelaezv06 <dpelaezv@unal.edu.co>
2025-10-30 11:53:39 -0500

    Proyecto para el ejercicio de la pagina 412

---
+++ entrega_4/ejercicio_412/src/main/java/com/mycompany/ejercicio_412/Ejercicio_412.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_412;
+
+/**
+ *
+ * @author daniel
+ */
+public class Ejercicio_412 {
+
+    public static void main(String[] args) {
+        Ventana_principal ventana = new Ventana_principal();
+        ventana.setVisible(true);
+        ventana.setResizable(false);
+    }
+}
