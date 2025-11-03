b8eb3e44bacf156a3d864072af4e8f1c58b8b37d
dpelaezv06 <dpelaezv@unal.edu.co>
2025-11-02 21:17:41 -0500

    Creacion del proyecto para ejercicio de la pagina 406

---
+++ ejercicio_406/src/main/java/com/mycompany/ejercicio_406/Ejercicio_406.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_406;
+
+/**
+ *
+ * @author daniel
+ */
+public class Ejercicio_406 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
