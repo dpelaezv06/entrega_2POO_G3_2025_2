235db544ea0aa62019e210d867a0b4d3a06f41b7
Julián <julperezca@unal.edu.co>
2025-11-04 11:42:54 -0500

    Ejercicio 418

---
+++ entrega_4/ejercicio_418/ejercicio_418/src/Programador.java
@@ -0,0 +1,23 @@
+package Excepciones;
+/**
+* Esta clase denominada Programador modela un integrante de un
+* equipo de programadores que participará en una maratón de
+* programación. Un programador cuenta con un nombre y apellidos.
+* @version 1.2/2020
+*/
+public class Programador {
+String nombre; /* Atributo que identifica el nombre de un
+programador */
+String apellidos; /* Atributo que identifica los apellidos de un
+programador */
+/**
+* Constructor de la clase Programador
+* @param nombre Parámetro que define el nombre del programador
+* @param apellidos Parámetro que define los apellidos del
+* programador
+*/
+Programador(String nombre, String apellidos) {
+this.nombre = nombre;
+this.apellidos = apellidos;
+}
+}
---
+++ entrega_4/ejercicio_418/ejercicio_418/src/ejercicio_418.java
@@ -0,0 +1,5 @@
+public class ejercicio_418 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
