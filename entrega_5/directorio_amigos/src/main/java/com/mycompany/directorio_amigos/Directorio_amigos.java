09293c0d211593206bc4331880235a2f57dd3d0f
dpelaezv06 <dpelaezv@unal.edu.co>
2025-11-11 20:06:43 -0500

    creacion del proyecto para la entrega 5

---
+++ entrega_5/directorio_amigos/src/main/java/com/mycompany/directorio_amigos/Directorio_amigos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.directorio_amigos;
+
+/**
+ *
+ * @author daniel
+ */
+public class Directorio_amigos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
